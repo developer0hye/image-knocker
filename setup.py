544b1006a66b415572089a5e24ef4d95f4076f55
yhkwon-DT01 <developer.0hye@gmail.com>
2021-09-05 17:53:25 +0900

    first upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import re
+
+with open("image_knocker/__init__.py", "r") as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        f.read(), re.MULTILINE
+    ).group(1)
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+setuptools.setup(
+    name="image_knocker",
+    version=version,
+    license='Apache',
+    python_requires=">=3.6",
+    author="Yonghye Kwon",
+    author_email="developer.0hye@gmail.com",
+    description="Knock your images before these make you painful",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/developer0hye/image-knocker",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    keywords=['image', 'img'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
